7787efe5d45a16ca3012ceddf5ad106f65cd0435
JacobReeder <jacob.reeder1@gmail.com>
2022-04-24 15:07:37 -0700

    inserted table data successfully//Issue 1 closed

--- db/connection.js
+++ db/connection.js
@@ -13,6 +13,16 @@ const db = mysql.createConnection(
       database: 'tracker'
     },
     console.log('Connected to the tracker database.')
+
   );
 
   module.exports = db;
+
+  // create connection to our db
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;*/
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,19 @@ INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
 
   (1,'Ronald', 'Firbank', 1, 1),
-  (2,'Rick', 'Astley', 2, 3),
-  (3,'Tony', 'Soprano', 3, 4)
+  (2,'Rick', 'Astley', 2, 0),
+  (3,'Tony', 'Soprano', 3, 0);
+
+  INSERT INTO departments (id, department_name)
+  VALUES 
+  
+  (5, 'Business');
+
+
+  INSERT INTO roles (id, title, salary, department_id)
+  VALUES 
+  
+  (1, 'Manager', 100000.00, 1);
 
 
 

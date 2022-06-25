f38d0519eab7cdbacae9e1ef9ea0ddce6d4c1840
JacobReeder <jacob.reeder1@gmail.com>
2022-06-25 15:44:03 -0700

    Code refactor and export: Added new pages

---
+++ Index.js
@@ -0,0 +1,67 @@
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+const {trackerPrompt, nextChoice, newDep, newRole} = require('./pages/prompt');
+
+console.log(`
+=================
+Employee Tracker
+=================
+`)
+
+function RunApp() {
+//   inquirer.prompt.trackerPrompt.then(result => {
+    
+/*  if ( === 'View Employee') {
+
+
+    }*/
+
+/*  if ( === 'Add an Employee') {
+
+
+    }*/
+  
+/*if ( === 'Update Employee') {
+
+   inguirer.prompt([
+
+    }*/
+  
+/* if ( === 'View Role') {
+
+    inguirer.prompt([
+
+}*/
+  
+ /*if ( === 'Add Role') {
+   inguirer.prompt([
+    }*/
+  
+/* if ( === 'View Department') {
+    inguirer.prompt([
+}*/
+
+ /* if ( === 'New Department') {
+      }*/
+  
+ /*if ( === 'DONE') {
+   inguirer.prompt([
+    }*/
+}
+
+
+
+
+
+
+
+         
+
+ 
+
+
+
+
+

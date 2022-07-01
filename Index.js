bb8d0cda5b7669b1eff986725b881365e2adf1cd
JacobReeder <jacob.reeder1@gmail.com>
2022-07-01 15:27:00 -0700

    Index almost finished;need to complete department functions

--- Index.js
+++ Index.js
@@ -4,6 +4,7 @@ const consoleTable = require('console.table');
 
 const {trackerPrompt, 
        nextChoice, 
+      // updateEmp
        newDep, 
        newRole} = require('./pages/prompt');
 
@@ -26,7 +27,7 @@ Employee Tracker
 `)
 
 function RunApp() {
-   // inquirer.prompt(menu).then(result => {
+   inquirer.prompt(trackerPrompt).then(result => {
     
   if (result.res === 'View Employee') {
        db.query(seeEmployee, (err, rows) => {
@@ -36,42 +37,73 @@ function RunApp() {
     }
 
   if (result.res === 'Add an Employee') {
-  //  inq.prompt().then(res => {
-   //   console.log(res);
-   //   const managerId = parseManager(res.manager)
-   //   const params = [res.firstName, res.lastName, res.role, managerId]
-
+     inq.prompt(nextChoice).then(res => {
+      console.log(res);
+     const managerId = parseManager(res.manager)
+     const params = [res.firstName, res.lastName, res.role, managerId]
+   
+       db.query(nextChoice, params, (err, result) => {
+            if(err) throw err;
+            console.log('Employee Added!');
+            start();
+          })          
+       })
     }
   
   if (result.res === 'Update Employee') {
-
-  // inguirer.prompt([
-
-    }
+    db.query(getEmployee, (err,data) => {
+     if (err) throw err;
+     console.log('-------------')
+     console.table(data)
+    })
+   // setTimeout(()=> {
+    //  inquirer.prompt(/*updateEmp*/).then(res => {
+     //   const employeeId = res.employeeId
+     //   const roleId = parseRole(res.roleName)
+     //   db.query(updateEmployee, [ employeeId, roleId ], (err, res) => {
+     //     console.log('Employee Role changed');
+     //   })
+    //  })
+   // }, 1000)
+ // }
   
   if (result.res === 'View Role') {
+    db.query(seeRoles, (err, res) => {
+      if (err) throw err;
+      console.log('------')
+      console.table(res);
+      RunApp();
+    }) 
+  }  
+   if (result.res === 'Add Role') {
+     inq.prompt(newRole).then(res => {
+      const role = res.name
+      const salary = res.salary
+      const department = res.department
 
-    //  inguirer.prompt([
-  }
-  
- if (result.res === 'Add Role') {
-  //  inguirer.prompt([
+      db.query(newRole, [role, salary, department], (err, res) => {
+        if (err) throw err;
+        console.log('Role added!')
+        RunApp();
+      })
+     })
     }
   
- if (result.res === 'View Department') {
+   /* if (result.res === 'View Department') {
    // inguirer.prompt([
-  }
+    }
 
-  if (result.res === 'New Department') {
-      }
+    if (result.res === 'New Department') {
+    }
   
-  if (result.res === 'DONE') {
-   //inguirer.prompt([
- }
- 
+    if (result.res === 'DONE') {
+     //inguirer.prompt([
+    }*/
+  }
 }
+)}
 
-
+RunApp();
 
 
 
